b158d7e759087dce9a9198a2c54cb86af476e4b9
Siavash Fathi <siavash.fathi@gmail.com>
2023-04-15 17:33:31 -0400

    Adding basic azure function

---
+++ api/message.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace api
+{
+    public class message
+    {
+        private readonly ILogger _logger;
+
+        public message(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<message>();
+        }
+
+        [Function("message")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
